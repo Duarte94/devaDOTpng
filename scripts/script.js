dac065f3e200561df756d0f1a9a336469d5872f8
Deva <140471222+Duarte94@users.noreply.github.com>
2025-03-06 19:20:19 -0300

    update scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 const filterButton = document.getElementById("filter-btn");
 const filterDrop = document.getElementById("filter-cont");
 
+const commTitle = document.getElementsByClassName("comm-card");
+const commDrop = document.getElementsByClassName("reference-drop");
+
 filterButton.addEventListener("click", function(event) {
     event.stopPropagation(); // Previene la propagación del clic para evitar cerrarlo inmediatamente
     toggleFilterDrop(); // Llama a la función para mostrar/ocultar
@@ -19,4 +22,23 @@ function toggleFilterDrop() {
     } else {
         filterDrop.style.display = "flex"; // Muestra si está oculto
     }
+}
+
+commTitle.addEventListener("click", function(event){
+  event.stopPropagation();
+  toggleCommDrop();
+});
+
+document.addEventListener("click", function(){
+  if (commDrop.style.display === "flex"){
+    commDrop.style.display = "none";
+  }
+});
+
+function toggleCommDrop(){
+  if (commDrop.style.display === "flex"){
+    commDrop.style.display = "none";
+  } else {
+    commDrop.style.display = "flex"
+  }
 }
